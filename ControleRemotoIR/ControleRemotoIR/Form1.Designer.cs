ea16d90f9c2cf11ce62fc6c05dd2a101fce88875
MaterLador <leandro84ribeiro@gmail.com>
2018-12-12 20:53:30 -0200

    First Upload
    Files sent successfully

---
+++ ControleRemotoIR/ControleRemotoIR/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ControleRemotoIR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btConectar = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbDadosRecebidos = new System.Windows.Forms.Label();
+            this.textBoxReceber = new System.Windows.Forms.TextBox();
+            this.lbComandoAntigo = new System.Windows.Forms.Label();
+            this.cbResetStart = new System.Windows.Forms.CheckBox();
+            this.btQMEH = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btConectar
+            // 
+            this.btConectar.Location = new System.Drawing.Point(197, 13);
+            this.btConectar.Name = "btConectar";
+            this.btConectar.Size = new System.Drawing.Size(75, 23);
+            this.btConectar.TabIndex = 0;
+            this.btConectar.Text = "Conectar";
+            this.btConectar.UseVisualStyleBackColor = true;
+            this.btConectar.Click += new System.EventHandler(this.btConectar_Click);
+            this.btConectar.Enter += new System.EventHandler(this.btConectar_Enter);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(117, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.Enter += new System.EventHandler(this.comboBox1_Enter);
+            // 
+            // lbDadosRecebidos
+            // 
+            this.lbDadosRecebidos.AutoSize = true;
+            this.lbDadosRecebidos.Location = new System.Drawing.Point(12, 47);
+            this.lbDadosRecebidos.Name = "lbDadosRecebidos";
+            this.lbDadosRecebidos.Size = new System.Drawing.Size(35, 13);
+            this.lbDadosRecebidos.TabIndex = 2;
+            this.lbDadosRecebidos.Text = "label1";
+            // 
+            // textBoxReceber
+            // 
+            this.textBoxReceber.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxReceber.Location = new System.Drawing.Point(12, 73);
+            this.textBoxReceber.Multiline = true;
+            this.textBoxReceber.Name = "textBoxReceber";
+            this.textBoxReceber.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxReceber.Size = new System.Drawing.Size(260, 176);
+            this.textBoxReceber.TabIndex = 3;
+            // 
+            // lbComandoAntigo
+            // 
+            this.lbComandoAntigo.AutoSize = true;
+            this.lbComandoAntigo.Location = new System.Drawing.Point(210, 47);
+            this.lbComandoAntigo.Name = "lbComandoAntigo";
+            this.lbComandoAntigo.Size = new System.Drawing.Size(35, 13);
+            this.lbComandoAntigo.TabIndex = 4;
+            this.lbComandoAntigo.Text = "label1";
+            // 
+            // cbResetStart
+            // 
+            this.cbResetStart.AutoSize = true;
+            this.cbResetStart.Checked = true;
+            this.cbResetStart.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbResetStart.Location = new System.Drawing.Point(77, 46);
+            this.cbResetStart.Name = "cbResetStart";
+            this.cbResetStart.Size = new System.Drawing.Size(79, 17);
+            this.cbResetStart.TabIndex = 5;
+            this.cbResetStart.Text = "Reset Start";
+            this.cbResetStart.UseVisualStyleBackColor = true;
+            // 
+            // btQMEH
+            // 
+            this.btQMEH.Location = new System.Drawing.Point(136, 13);
+            this.btQMEH.Name = "btQMEH";
+            this.btQMEH.Size = new System.Drawing.Size(55, 23);
+            this.btQMEH.TabIndex = 6;
+            this.btQMEH.Text = "QMEH?";
+            this.btQMEH.UseVisualStyleBackColor = true;
+            this.btQMEH.Click += new System.EventHandler(this.btQMEH_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 17);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btQMEH);
+            this.Controls.Add(this.cbResetStart);
+            this.Controls.Add(this.lbComandoAntigo);
+            this.Controls.Add(this.textBoxReceber);
+            this.Controls.Add(this.lbDadosRecebidos);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btConectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConectar;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lbDadosRecebidos;
+        private System.Windows.Forms.TextBox textBoxReceber;
+        private System.Windows.Forms.Label lbComandoAntigo;
+        private System.Windows.Forms.CheckBox cbResetStart;
+        private System.Windows.Forms.Button btQMEH;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ControleRemotoIR/ControleRemotoIR/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.IO.Ports; 	// necessário para ter acesso as portas
+
+namespace ControleRemotoIR
+{
+    public partial class Form1 : Form
+    {
+        //*Importando DLL para clicar com o Mouse* https://social.msdn.microsoft.com/Forums/vstudio/pt-BR/241c6422-b631-41d3-b77c-12f2dda63bd3/mouse-mover-e-clicar?forum=vscsharppt
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+        //****************************************
+
+        string RxString;
+        string comandoAntigo = "";
+        bool testaHW = false;
+        int mouseX = 0, mouseY = 0, aumento = 1; //variaveis para controlar velocidade do mouse
+        byte tentativasConexao = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizaListaCOMs();
+            autoConecta();
+        }
+
+        private void autoConecta()
+        {
+            textBoxReceber.AppendText(" serialPort1.IsOpen = " + serialPort1.IsOpen.ToString() + "\n");
+            if (serialPort1.IsOpen == false)
+            {
+                try
+                {
+                    textBoxReceber.AppendText("Conectando...\n");
+                    //textBoxReceber.AppendText("\n");
+                    textBoxReceber.AppendText("Recebendo porta COM...\n");
+                    serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                    textBoxReceber.AppendText(" Porta COM recebida: " + serialPort1.PortName.ToString() + "\n");
+                    textBoxReceber.AppendText("Abrindo Porta...\n");
+                    serialPort1.Open();
+                    textBoxReceber.AppendText(" serialPort1.IsOpen = " + serialPort1.IsOpen.ToString() + "\n");
+                    textBoxReceber.AppendText("Auto recetar CONTROLE = " + cbResetStart.Checked.ToString() + "\n");
+                    if (cbResetStart.Checked)
+                    {
+                        textBoxReceber.AppendText(" Atribuindo DtrEnable = true\n");
+                        serialPort1.DtrEnable = true;
+                        textBoxReceber.AppendText(" Atribuindo DtrEnable = false\n");
+                        textBoxReceber.AppendText("Aguardando Reset = ");
+                        serialPort1.DtrEnable = false;
+                    }
+                    else
+                    {//se nao tiver o auto reset a placa não envia CONTROLE quando recebe conexão. Por isso criou-se esse else Uma maneira de verificar
+                        //Se conectou certinho
+                        textBoxReceber.AppendText("QMEH?\n");
+                        textBoxReceber.AppendText("Aguardando resposta = ");
+                        if (serialPort1.IsOpen == true)          //porta está aberta
+                            serialPort1.Write("QMEH");
+                    }
+
+                }
+                catch
+                {
+                    return;
+                }
+                if (serialPort1.IsOpen)
+                {
+                    btConectar.Text = "Desconectar";
+                    comboBox1.Enabled = false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    textBoxReceber.AppendText("Desconectando...\n");
+                    textBoxReceber.AppendText("Fechando comunicação com porta serial\n");
+                    serialPort1.Close();
+                    comboBox1.Enabled = true;
+                    btConectar.Text = "Conectar";
+                }
+                catch
+                {
+                    return;
+                }
+            }
+        }
+
+        private void btConectar_Click(object sender, EventArgs e)
+        {
+            autoConecta();
+        }
+
+        void atualizaListaCOMs()
+        {
+            int i;
+            bool quantDiferente;    //flag para sinalizar que a quantidade de portas mudou
+
+            i = 0;
+            quantDiferente = false;
+
+            //se a quantidade de portas mudou
+            if (comboBox1.Items.Count == SerialPort.GetPortNames().Length) //incluir using System.IO.Ports; para acesso ao SerialPort
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    if (comboBox1.Items[i++].Equals(s) == false)
+                    {
+                        quantDiferente = true;
+                    }
+                }
+            }
+            else
+            {
+                quantDiferente = true;
+            }
+
+            //Se não foi detectado diferença
+            if (quantDiferente == false)
+            {
+                return;                     //retorna
+            }
+
+            //limpa comboBox
+            comboBox1.Items.Clear();
+
+            //adiciona todas as COM diponíveis na lista
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+            //seleciona a primeira posição da lista
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_Enter(object sender, EventArgs e)
+        {
+            atualizaListaCOMs();
+        }
+
+        private void btConectar_Enter(object sender, EventArgs e)
+        {
+            atualizaListaCOMs();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            RxString = serialPort1.ReadLine();  //Le dados na serial até "/n"
+            this.Invoke(new EventHandler(trataDadoRecebido));   //chama outra thread para escrever o dado no text box
+        }
+
+        private void trataDadoRecebido(object sender, EventArgs e)
+        {
+            if (RxString == "CONTROLE\r")
+            {
+                lbDadosRecebidos.Text = "OK";
+                textBoxReceber.AppendText("OK\n");
+                testaHW = true;
+                return;
+            }
+            lbDadosRecebidos.Text = RxString;
+            string valordoLabel = lbDadosRecebidos.Text;   //variavel para acrescentar \n new line
+            valordoLabel += "\n";
+            textBoxReceber.AppendText(valordoLabel);
+            if (testaHW)  // Se testa recebe true quando recebe do arduino a informação "CONTROLE"
+                comandoparateclado(RxString);
+            else
+            {
+                if(tentativasConexao > 0)
+                {
+                    string msg = "Tentativas de comunicação: ";
+                    textBoxReceber.AppendText(string.Concat(msg,tentativasConexao.ToString())+"\n");
+                    textBoxReceber.AppendText("QMEH foi enviado pela serial.");
+                    btQMEH.PerformClick();
+                    tentativasConexao--;
+                    return;
+                }
+                MessageBox.Show("Comandos recebidos pela serial não parecem ser do controle. \rEsperado: CONTROLE\rRecebido: " + RxString);
+                
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen == true)  // se porta aberta
+                serialPort1.Close();        	//fecha a porta
+        }
+
+        void comandoparateclado(string comando)
+        {
+            if (comando.TrimEnd() != "FFFFFFFF")
+            {
+                comandoAntigo = comando;
+                lbComandoAntigo.Text = comando;
+                mouseX = 0;
+                mouseY = 0;
+                aumento = 1;
+            }
+            else
+            {
+                comando = comandoAntigo;
+                mouseX += aumento;
+                mouseY += aumento;
+                aumento++;
+            }
+
+
+            switch (comando.TrimEnd())
+            {
+                case "FFA25D": //CH-
+                    SendKeys.Send("{ESC}");
+                    break;
+                case "FF629D": //CH
+                    System.Diagnostics.Process.Start("chrome.exe", "https://www.netflix.com/");
+                    SendKeys.Send("{F11}");
+                    break;
+                case "FFE21D": //CH+
+                    
+                    break;
+//-------------------------------------------------------------
+                case "FF22DD": // |<<
+                    SendKeys.Send("+({LEFT})");
+                    break;
+                case "FF02FD": // >>|
+                    SendKeys.Send("+({RIGHT})");
+                    break;
+                case "FFC23D": // >|| Play/Pause
+                    SendKeys.Send("{ENTER}");
+                    Thread.Sleep(200);
+                    break;
+//-------------------------------------------------------------
+                case "FFE01F": // -
+                    SendKeys.Send("{Down}");
+                    break;
+                case "FFA857": // +
+                    SendKeys.Send("{UP}");
+                    break;
+                case "FF906F": // EQ
+                    SendKeys.Send("f");
+                    break;
+//-------------------------------------------------------------
+                case "FF6897": // 0
+                    mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+                    break;
+                case "FF9867": // 100+
+                    SendKeys.Send("");
+                    break;
+                case "FFB04F": // 200+
+                    SendKeys.Send("f");
+                    break;
+//-------------------------------------------------------------
+                case "FF30CF": // 1
+                    this.Cursor = new Cursor(Cursor.Current.Handle);
+                    Cursor.Position = new Point(Cursor.Position.X - mouseX, Cursor.Position.Y - mouseY);
+                    break;
+                case "FF18E7": // 2                    
+                    this.Cursor = new Cursor(Cursor.Current.Handle);
+                    Cursor.Position = new Point(Cursor.Position.X - 0, Cursor.Position.Y - mouseY);
+                    break;
+                case "FF7A85": // 3
+                    this.Cursor = new Cursor(Cursor.Current.Handle);
+                    Cursor.Position = new Point(Cursor.Position.X + mouseX, Cursor.Position.Y - mouseY);
+                    break;
+//-------------------------------------------------------------
+                case "FF10EF": // 4
+                    this.Cursor = new Cursor(Cursor.Current.Handle);
+                    Cursor.Position = new Point(Cursor.Position.X - mouseX, Cursor.Position.Y - 0);
+                    break;
+                case "FF38C7": // 5                    
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    break;
+                case "FF5AA5": // 6
+                    this.Cursor = new Cursor(Cursor.Current.Handle);
+                    Cursor.Position = new Point(Cursor.Position.X + mouseX, Cursor.Position.Y - 0);
+                    break;
+                //-------------------------------------------------------------
+                case "FF42BD": // 7
+                    this.Cursor = new Cursor(Cursor.Current.Handle);
+                    Cursor.Position = new Point(Cursor.Position.X - mouseX, Cursor.Position.Y + mouseY);
+                    break;
+                case "FF4AB5": // 8                    
+                    this.Cursor = new Cursor(Cursor.Current.Handle);
+                    Cursor.Position = new Point(Cursor.Position.X - 0, Cursor.Position.Y + mouseY);
+                    break;
+                case "FF52AD": // 9
+                    this.Cursor = new Cursor(Cursor.Current.Handle);
+                    Cursor.Position = new Point(Cursor.Position.X + mouseX, Cursor.Position.Y + mouseY);
+                    break;
+                    //-------------------------------------------------------------
+            }
+        }
+
+        private void btQMEH_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)          //porta está aberta
+                serialPort1.Write("QMEH");  //envia o texto 
+            else
+                textBoxReceber.AppendText("Porta de COM não esta aberta.\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Set the Current cursor, move the cursor's Position,
+            // and set its clipping rectangle to the form. 
+
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(Cursor.Position.X - 0, Cursor.Position.Y - 2);
+            //Cursor.Clip = new Rectangle(this.Location, this.Size);
+
+        }
+    }
+}
+
+
+/* Mapa do controle
+FFA25D	FF629D	FFE21D		CH-	    CH	    CH+
+FF22DD	FF02FD	FFC23D		|<<	    >>|	    >||
+FFE01F	FFA857	FF906F		-	     +   	 EQ
+FF6897	FF9867	FFB04F		0	    100+	200+
+FF30CF	FF18E7	FF7A85		1	     2	     3
+FF10EF	FF38C7	FF5AA5		4	     5	     6
+FF42BD	FF4AB5	FF52AD		7	     8	     9
+
+    https://msdn.microsoft.com/pt-br/library/system.windows.forms.sendkeys.send(v=vs.110).aspx
+    https://pt.wikipedia.org/wiki/ASCII
+    https://support.microsoft.com/pt-br/help/12445/windows-keyboard-shortcuts
+    http://www.techtudo.com.br/noticias/noticia/2014/08/lista-tem-16-atalhos-e-comandos-especiais-para-netflix-no-pc.html
+
+*/
